ed8741c4da10506612868aab1553cbc954e8e4a8
rajarai37 <rajarai37@gmail.com>
2022-03-21 12:29:03 +0530

    order fetch and edit

--- BackEnd/routes/order.js
+++ BackEnd/routes/order.js
@@ -1,9 +1,7 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-const jwt = require("jsonwebtoken");
 const Order = require("../model/orders");
 const router = express.Router();
-const bcrypt = require("bcrypt");
 SECRET = "RESTAPI";
 
 //============================Create Order API=====================================
@@ -15,7 +13,7 @@ router.post("/create", async (req, res) => {
 
     const order = await Order.create({
       user: req.user,
-      items: req.body,
+      items:req.body.items,
       status: situation[state],
     });
     // console.log(req.body);
@@ -29,6 +27,48 @@ router.post("/create", async (req, res) => {
       status: "failed",
       message: e.message,
     });
+  }
+});
+
+// ---------------FETCH ORDERS---------------
+router.get("/create", async(req,res) => {
+  try{
+    const order = await Order.find({user:req.user});
+    res.status(200).json({
+        status: "success",
+        order
+    })
+  }catch(e){
+        console.log(e);
+        return res.status(500).json({
+            status:"Failed",
+            message:e.message
+        })
+    }
+});
+
+
+// ============================ EDIT POSTS =====================================
+router.put("/create/:id", async(req,res) => {
+  try{
+     const order = await Order.updateOne({_id:req.params.id},{status:"Cancelled"} );
+     if (order.modifiedCount>0){
+          return res.status(200).json({
+              status:"Success",
+              message:"Order Cancelled"
+          })
+     }else{
+         return res.status(401).json({
+             status:"Failed",
+             message:"Not authorized to cancel the order"
+         })
+     }
+  }catch(e){
+      console.log(e);
+      return res.status(500).json({
+          status:"Failed",
+          message:e.message
+      })
   }
 });
 
